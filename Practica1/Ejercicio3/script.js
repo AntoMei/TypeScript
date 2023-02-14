105ff3d63a126d09d24a4423ff33318f94ff2baf
AntoMei <antonio.meirosu444@gmail.com>
2023-02-14 10:10:29 +0100

    Realizado Ejercicio3

--- Practica1/Ejercicio3/script.js
+++ Practica1/Ejercicio3/script.js
@@ -1,13 +1,12 @@
-var num1Input = document.getElementById("num1");
-var num2Input = document.getElementById("num2");
-var sumButton = document.getElementById("sum-button");
-sumButton.addEventListener("click", function () {
-    var num1 = parseFloat(num1Input.value);
-    var num2 = parseFloat(num2Input.value);
-    var sum = num1 + num2;
-    var sumInput = document.createElement("input");
-    sumInput.setAttribute("type", "text");
-    sumInput.value = sum.toString();
-    sumInput.setAttribute("disabled", "true");
-    document.body.appendChild(sumInput);
-});
+function sumar() {
+    var num1 = parseFloat(document.getElementById("num1").value);
+    var num2 = parseFloat(document.getElementById("num2").value);
+    var resultado = num1 + num2;
+    var resultadoField = document.createElement("input");
+    resultadoField.type = "number";
+    resultadoField.value = resultado.toString();
+    resultadoField.disabled = true;
+    var resultadoContainer = document.getElementById("resultado-container");
+    resultadoContainer.innerHTML = "";
+    resultadoContainer.appendChild(resultadoField);
+}
--- Practica1/Ejercicio3/script.ts
+++ Practica1/Ejercicio3/script.ts
@@ -1,14 +1,14 @@
-const num1Input = document.getElementById("num1") as HTMLInputElement;
-const num2Input = document.getElementById("num2") as HTMLInputElement;
-const sumButton = document.getElementById("sum-button") as HTMLButtonElement;
+function sumar() {
+  const num1 = parseFloat((<HTMLInputElement>document.getElementById("num1")).value);
+  const num2 = parseFloat((<HTMLInputElement>document.getElementById("num2")).value);
+  const resultado = num1 + num2;
 
-sumButton.addEventListener("click", () => {
-  const num1 = parseFloat(num1Input.value);
-  const num2 = parseFloat(num2Input.value);
-  const sum = num1 + num2;
-  const sumInput = document.createElement("input");
-  sumInput.setAttribute("type", "text");
-  sumInput.value = sum.toString();
-  sumInput.setAttribute("disabled", "true");
-  document.body.appendChild(sumInput);
-});
+  const resultadoField = document.createElement("input");
+  resultadoField.type = "number";
+  resultadoField.value = resultado.toString();
+  resultadoField.disabled = true;
+
+  const resultadoContainer = document.getElementById("resultado-container");
+  resultadoContainer.innerHTML = "";
+  resultadoContainer.appendChild(resultadoField);
+}
